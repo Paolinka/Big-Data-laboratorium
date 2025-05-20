77610ed3a4f330e1b327f1eae73eb58aaed65f30
Paolinka <paolik27@gmail.com>
2025-05-20 13:55:57 +0200

    Skonczona lista 8

--- lista 7/lista 7.py
+++ lista 7/lista 7.py
@@ -211,130 +211,193 @@ def krzywa_roc(y_test, y_scores, model_name):
     plt.show()
 
 
-# Zadanie 2
-iris = datasets.load_iris()
-zbadaj_strukture(iris)
+# # Zadanie 2
+# iris = datasets.load_iris()
+# zbadaj_strukture(iris)
 
-# Konwersja do DataFrame
-iris_df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
-iris_df['target'] = iris.target
+# # Konwersja do DataFrame
+# iris_df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+# iris_df['target'] = iris.target
 
-# Przygotowanie danych do klasyfikacji
-# Usunięcie brakujących wartości 
-iris_df.dropna(inplace=True) 
+# # Przygotowanie danych do klasyfikacji
+# # Usunięcie brakujących wartości 
+# iris_df.dropna(inplace=True) 
 
-# Usunięcie duplikatów 
-iris_df.drop_duplicates(inplace=True)
+# # Usunięcie duplikatów 
+# iris_df.drop_duplicates(inplace=True)
 
-# Przetwarzanie danych
-iris_df = przetwarzanie_danych(iris_df)
+# # Przetwarzanie danych
+# iris_df = przetwarzanie_danych(iris_df)
 
-# Trenowanie i ocena modeli
-iris_results, _ = zbuduj_modele_i_wykonaj_ocene(iris_df)
+# # Trenowanie i ocena modeli
+# iris_results, _ = zbuduj_modele_i_wykonaj_ocene(iris_df)
 
-# Wnioski:
-# Wszystkie modele osiągnęły idealną dokładność 1.00, co sugeruje, że doskonale radzą sobie z danymi.
-# W macierzy pomyłek nie wystąpiły żadne błędne klasyfikacje.
-
-
-
-# Zadanie 3
-breast_cancer = datasets.load_breast_cancer()
-zbadaj_strukture(breast_cancer)
-
-# Konwersja do DataFrame    
-breast_cancer_df = pd.DataFrame(data=breast_cancer.data, columns=breast_cancer.feature_names)
-breast_cancer_df['target'] = breast_cancer.target
-
-# Przygotowanie danych do klasyfikacji
-# Usunięcie brakujących wartości
-breast_cancer_df.dropna(inplace=True)
-
-# Usunięcie duplikatów
-breast_cancer_df.drop_duplicates(inplace=True)
-
-# Przetwarzanie danych
-breast_cancer_df = przetwarzanie_danych(breast_cancer_df)
-
-# Trenowanie i ocena modeli
-breast_cancer_results, breast_cancer_features = zbuduj_modele_i_wykonaj_ocene(breast_cancer_df)
-
-# Wnioski:
-# Wszystkie modele osiągnęły bardzo wysoką dokładność (w okolicach 0.95), co sugeruje, że dobrze radzą sobie z danymi.
-# W przypadku klasyfikacji raka piersi groźniejsze są błędne klasyfikacje typu 1 (False Negative), ponieważ mogą prowadzić do nieodpowiedniego leczenia pacjentów.
+# # Wnioski:
+# # Wszystkie modele osiągnęły idealną dokładność 1.00, co sugeruje, że doskonale radzą sobie z danymi.
+# # W macierzy pomyłek nie wystąpiły żadne błędne klasyfikacje.
 
 
-# Zadanie 4
-digits = datasets.load_digits()
-zbadaj_strukture(digits)
 
-# Spłaszczenie obrazów
-n_samples = len(digits.images)
-digits_flattened = digits.images.reshape((n_samples, -1))
+# # Zadanie 3
+# breast_cancer = datasets.load_breast_cancer()
+# zbadaj_strukture(breast_cancer)
 
-# Konwersja do DataFrame
-digits_df = pd.DataFrame(data=digits_flattened)
-digits_df['target'] = digits.target
+# # Konwersja do DataFrame    
+# breast_cancer_df = pd.DataFrame(data=breast_cancer.data, columns=breast_cancer.feature_names)
+# breast_cancer_df['target'] = breast_cancer.target
 
-# Standaryzacja wartości pikseli
-scaler = StandardScaler()
-feature_columns = digits_df.drop('target', axis=1).columns
-digits_df[feature_columns] = scaler.fit_transform(digits_df[feature_columns])
+# # Przygotowanie danych do klasyfikacji
+# # Usunięcie brakujących wartości
+# breast_cancer_df.dropna(inplace=True)
 
-# Przygotowanie danych do klasyfikacji
-# Usunięcie brakujących wartości
-digits_df.dropna(inplace=True)
+# # Usunięcie duplikatów
+# breast_cancer_df.drop_duplicates(inplace=True)
 
-# Usunięcie duplikatów
-digits_df.drop_duplicates(inplace=True)
+# # Przetwarzanie danych
+# breast_cancer_df = przetwarzanie_danych(breast_cancer_df)
 
-# Przetwarzanie danych
-digits_df = przetwarzanie_danych(digits_df)
+# # Trenowanie i ocena modeli
+# breast_cancer_results, breast_cancer_features = zbuduj_modele_i_wykonaj_ocene(breast_cancer_df)
 
-# Trenowanie i ocena modeli
-digits_results, _ = zbuduj_modele_i_wykonaj_ocene(digits_df, chosen_models=['SVM'])
-
-# Analiza wyników dla zbioru digits
-print("\nAnaliza wyników dla zbioru digits:")
-
-# Pobranie wyników dla modelu SVM
-svm_results = digits_results['SVM']
-confusion = svm_results['confusion_matrix']
-
-# Analiza błędów klasyfikacji
-misclassification_counts = np.sum(confusion, axis=1) - np.diag(confusion)
-
-# Wyświetlenie liczby błędnych klasyfikacji dla każdej cyfry
-print("\nLiczba błędnych klasyfikacji dla każdej cyfry:")
-for digit, count in enumerate(misclassification_counts):
-    print(f"Cyfra {digit}: {count} błędnych klasyfikacji")
-
-# Wnioski:
-# Model SVM osiągnął bardzo wysoką dokładność, co sugeruje, że bardzo dobrze radzi sobie z danymi.
-# W przypadku zbioru digits, najczęściej błędnie klasyfikowane cyfry to 9, 8 i 5.
+# # Wnioski:
+# # Wszystkie modele osiągnęły bardzo wysoką dokładność (w okolicach 0.95), co sugeruje, że dobrze radzą sobie z danymi.
+# # W przypadku klasyfikacji raka piersi groźniejsze są błędne klasyfikacje typu 1 (False Negative), ponieważ mogą prowadzić do nieodpowiedniego leczenia pacjentów.
 
 
-# Zadanie 5
-titanic = pd.read_csv('lista 7\Titanic-Dataset.csv')
-print(titanic.head())
-print(titanic.info())
-print(titanic.describe())
+# # Zadanie 4
+# digits = datasets.load_digits()
+# zbadaj_strukture(digits)
 
-# Przygotowanie danych do klasyfikacji
-titanic = przetwarzanie_danych(titanic)
-titanic.dropna(inplace=True)  # Usunięcie brakujących wartości
-titanic.drop_duplicates(inplace=True)  # Usunięcie duplikatów
-titanic.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)  # Usunięcie niepotrzebnych kolumn
+# # Spłaszczenie obrazów
+# n_samples = len(digits.images)
+# digits_flattened = digits.images.reshape((n_samples, -1))
 
-# Standaryzacja wartości
-scaler = StandardScaler()
-feature_columns = titanic.drop('Survived', axis=1).columns
-titanic[feature_columns] = scaler.fit_transform(titanic[feature_columns])
+# # Konwersja do DataFrame
+# digits_df = pd.DataFrame(data=digits_flattened)
+# digits_df['target'] = digits.target
 
-results_titanic, features_titanic = zbuduj_modele_i_wykonaj_ocene(titanic, target='Survived', roc_curve=True)
+# # Standaryzacja wartości pikseli
+# scaler = StandardScaler()
+# feature_columns = digits_df.drop('target', axis=1).columns
+# digits_df[feature_columns] = scaler.fit_transform(digits_df[feature_columns])
+
+# # Przygotowanie danych do klasyfikacji
+# # Usunięcie brakujących wartości
+# digits_df.dropna(inplace=True)
+
+# # Usunięcie duplikatów
+# digits_df.drop_duplicates(inplace=True)
+
+# # Przetwarzanie danych
+# digits_df = przetwarzanie_danych(digits_df)
+
+# # Trenowanie i ocena modeli
+# digits_results, _ = zbuduj_modele_i_wykonaj_ocene(digits_df, chosen_models=['SVM'])
+
+# # Analiza wyników dla zbioru digits
+# print("\nAnaliza wyników dla zbioru digits:")
+
+# # Pobranie wyników dla modelu SVM
+# svm_results = digits_results['SVM']
+# confusion = svm_results['confusion_matrix']
+
+# # Analiza błędów klasyfikacji
+# misclassification_counts = np.sum(confusion, axis=1) - np.diag(confusion)
+
+# # Wyświetlenie liczby błędnych klasyfikacji dla każdej cyfry
+# print("\nLiczba błędnych klasyfikacji dla każdej cyfry:")
+# for digit, count in enumerate(misclassification_counts):
+#     print(f"Cyfra {digit}: {count} błędnych klasyfikacji")
+
+# # Wnioski:
+# # Model SVM osiągnął bardzo wysoką dokładność, co sugeruje, że bardzo dobrze radzi sobie z danymi.
+# # W przypadku zbioru digits, najczęściej błędnie klasyfikowane cyfry to 9, 8 i 5.
+
+
+# # Zadanie 5
+# titanic = pd.read_csv('lista 7\Titanic-Dataset.csv')
+# print(titanic.head())
+# print(titanic.info())
+# print(titanic.describe())
+
+# # Przygotowanie danych do klasyfikacji
+# titanic = przetwarzanie_danych(titanic)
+# titanic.dropna(inplace=True)  # Usunięcie brakujących wartości
+# titanic.drop_duplicates(inplace=True)  # Usunięcie duplikatów
+# titanic.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)  # Usunięcie niepotrzebnych kolumn
+
+# # Standaryzacja wartości
+# scaler = StandardScaler()
+# feature_columns = titanic.drop('Survived', axis=1).columns
+# titanic[feature_columns] = scaler.fit_transform(titanic[feature_columns])
+
+# results_titanic, features_titanic = zbuduj_modele_i_wykonaj_ocene(titanic, target='Survived', roc_curve=True)
+
+# # Wyświetlenie najważniejszych cech dla każdego modelu
+# for model_name, feature_importance in features_titanic.items():
+#     if feature_importance is not None:
+#         sorted_features = sorted(feature_importance.items(), key=lambda x: x[1], reverse=True)
+#         print(f"\nNajważniejsze cechy dla modelu {model_name}:")
+#         for feature, importance in sorted_features[:5]:  # Top 5 cech
+#             print(f"{feature}: {importance:.4f}")
+#     else:
+#         print(f"\nModel {model_name} nie ma dostępnej ważności cech.")
+
+
+# # Wnioski:
+# # Wszystkie modele osiągnęły całkiem wysoką dokładność (w okolicach 0.8), ale KNN i Logistic Regression były najlepsze.
+# # W przypadku zbioru Titanic, najważniejsze cechy to sex, age, fare i class.
+
+# # Zadanie 6
+# heart_disease = pd.read_csv('lista 7\heart_failure_clinical_records_dataset.csv')
+# print(heart_disease.head())
+# print(heart_disease.info())
+# print(heart_disease.describe())
+
+# # Przygotowanie danych do klasyfikacji
+# heart_disease = przetwarzanie_danych(heart_disease)
+# heart_disease.dropna(inplace=True)  # Usunięcie brakujących wartości
+# heart_disease.drop_duplicates(inplace=True)  # Usunięcie duplikatów
+# heart_disease.drop(['time'], axis=1, inplace=True)  # Usunięcie niepotrzebnych kolumn
+
+# # Standaryzacja wartości
+# scaler = StandardScaler()
+# feature_columns = heart_disease.drop('DEATH_EVENT', axis=1).columns
+# heart_disease[feature_columns] = scaler.fit_transform(heart_disease[feature_columns])
+
+# # Trenowanie i ocena modeli
+# results_heart_disease, features_heart_disease = zbuduj_modele_i_wykonaj_ocene(heart_disease, target='DEATH_EVENT')
+
+# # Wyświetlenie najważniejszych cech dla każdego modelu
+# for model_name, feature_importance in features_heart_disease.items():
+#     if feature_importance is not None:
+#         sorted_features = sorted(feature_importance.items(), key=lambda x: x[1], reverse=True)
+#         print(f"\nNajważniejsze cechy dla modelu {model_name}:")
+#         for feature, importance in sorted_features[:5]:  # Top 5 cech
+#             print(f"{feature}: {importance:.4f}")
+#     else:
+#         print(f"\nModel {model_name} nie ma dostępnej ważności cech.")
+
+# # Wnioski:
+# # Wszystkie modele osiągnęły przeciętną dokładność, ale KNN i Logistic Regression były najlepsze.
+# # W przypadku zbioru heart_disease, najważniejsze cechy to age, ejection_fraction, serum_creatinine.
+# # Błędy w klasyfikacji mogą być spowodowane faktem, że target to śmierć pacjenta, a nie choroba serca.
+
+
+# Zadanie 1
+credit_card_data = pd.read_csv('lista 8\Credit_Card.csv')
+
+# Usuwanie brakujących wartości
+credit_card_data = credit_card_data.dropna()
+credit_card_data = credit_card_data.drop_duplicates()
+
+# Kodowanie zmiennych kategorycznych
+credit_card_data = przetwarzanie_danych(credit_card_data)
+
+res, feat = zbuduj_modele_i_wykonaj_ocene(credit_card_data, target='LIMIT_BAL')
 
 # Wyświetlenie najważniejszych cech dla każdego modelu
-for model_name, feature_importance in features_titanic.items():
+for model_name, feature_importance in feat.items():
     if feature_importance is not None:
         sorted_features = sorted(feature_importance.items(), key=lambda x: x[1], reverse=True)
         print(f"\nNajważniejsze cechy dla modelu {model_name}:")
@@ -343,42 +406,3 @@ for model_name, feature_importance in features_titanic.items():
     else:
         print(f"\nModel {model_name} nie ma dostępnej ważności cech.")
 
-
-# Wnioski:
-# Wszystkie modele osiągnęły całkiem wysoką dokładność (w okolicach 0.8), ale KNN i Logistic Regression były najlepsze.
-# W przypadku zbioru Titanic, najważniejsze cechy to sex, age, fare i class.
-
-# Zadanie 6
-heart_disease = pd.read_csv('lista 7\heart_failure_clinical_records_dataset.csv')
-print(heart_disease.head())
-print(heart_disease.info())
-print(heart_disease.describe())
-
-# Przygotowanie danych do klasyfikacji
-heart_disease = przetwarzanie_danych(heart_disease)
-heart_disease.dropna(inplace=True)  # Usunięcie brakujących wartości
-heart_disease.drop_duplicates(inplace=True)  # Usunięcie duplikatów
-heart_disease.drop(['time'], axis=1, inplace=True)  # Usunięcie niepotrzebnych kolumn
-
-# Standaryzacja wartości
-scaler = StandardScaler()
-feature_columns = heart_disease.drop('DEATH_EVENT', axis=1).columns
-heart_disease[feature_columns] = scaler.fit_transform(heart_disease[feature_columns])
-
-# Trenowanie i ocena modeli
-results_heart_disease, features_heart_disease = zbuduj_modele_i_wykonaj_ocene(heart_disease, target='DEATH_EVENT')
-
-# Wyświetlenie najważniejszych cech dla każdego modelu
-for model_name, feature_importance in features_heart_disease.items():
-    if feature_importance is not None:
-        sorted_features = sorted(feature_importance.items(), key=lambda x: x[1], reverse=True)
-        print(f"\nNajważniejsze cechy dla modelu {model_name}:")
-        for feature, importance in sorted_features[:5]:  # Top 5 cech
-            print(f"{feature}: {importance:.4f}")
-    else:
-        print(f"\nModel {model_name} nie ma dostępnej ważności cech.")
-
-# Wnioski:
-# Wszystkie modele osiągnęły przeciętną dokładność, ale KNN i Logistic Regression były najlepsze.
-# W przypadku zbioru heart_disease, najważniejsze cechy to age, ejection_fraction, serum_creatinine.
-# Błędy w klasyfikacji mogą być spowodowane faktem, że target to śmierć pacjenta, a nie choroba serca.
--- lista 8/lista 8.py
+++ lista 8/lista 8.py
@@ -1,26 +1,14 @@
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
-from sklearn.datasets import load_iris
-from sklearn.cluster import KMeans, MeanShift, AgglomerativeClustering
+from sklearn.datasets import load_iris, load_wine, fetch_olivetti_faces, load_diabetes
+from sklearn.cluster import KMeans, MeanShift, AgglomerativeClustering, DBSCAN
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from sklearn.mixture import GaussianMixture
+from sklearn.decomposition import PCA
+from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bouldin_score
 
-
-def zbadaj_strukture(dane):
-    """
-    Funkcja bada strukturę ramki danych
-    Parametry:
-    - ramka danych
-    Zwraca:
-    - None
-    """
-
-    print("Pierwsze kilka wierszy danych:")
-    print(dane.data[:5])
-    print("\nPierwsze kilka etykiet:")
-    print(dane.target[:5])
-    print("\nStruktura danych:")
-    print(dane.DESCR)
 
 
 def kodowanie_zmiennych_kategorycznych(dane):
@@ -57,9 +45,6 @@ def K_Means_clustering(dane, feature_1: str, feature_2: str, n_clusters):
     - None
     """
 
-    # Wybór cech do grupowania 
-    X = dane[[feature_1, feature_2]]
-
     n = len(n_clusters) if isinstance(n_clusters, list) else 1
     n_rows = n//2 if n % 2 == 0 else (n//2) + 1
 
@@ -68,13 +53,13 @@ def K_Means_clustering(dane, feature_1: str, feature_2: str, n_clusters):
     
     for i, k in enumerate(n_clusters if isinstance(n_clusters, list) else [n_clusters]):
         kmeans = KMeans(n_clusters=k, random_state=42)
-        kmeans.fit(X) 
+        kmeans.fit(dane) 
     
         # Przewidywanie przynależności do klastrów dla każdej próbki 
         labels = kmeans.labels_ 
     
         # Wyświetlenie wyników 
-        axes[i].scatter(X[feature_1], X[feature_2], c=labels, cmap='viridis', alpha=0.5) 
+        axes[i].scatter(dane[feature_1], dane[feature_2], c=labels, cmap='viridis', alpha=0.5) 
         axes[i].scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], marker='x', s=200, c='red') 
         axes[i].set_xlabel(feature_1) 
         axes[i].set_ylabel(feature_2) 
@@ -87,19 +72,16 @@ def K_Means_clustering(dane, feature_1: str, feature_2: str, n_clusters):
     
     
 def Mean_Shift_clustering(dane, feature_1: str, feature_2: str):
-    # Wybór cech do grupowania 
-    X = dane[[feature_1, feature_2]]
-
     
     # Implementacja metody Mean Shift 
     ms = MeanShift(max_iter=1000)
     print('Mean Shift - work in progress...')
-    ms.fit(X) 
+    ms.fit(dane) 
     cluster_centers = ms.cluster_centers_ 
     
     # Zwizualizowanie wyników 
     plt.figure(figsize=(10, 7)) 
-    plt.scatter(X[feature_1], X[feature_2], c=ms.labels_, cmap='viridis') 
+    plt.scatter(dane[feature_1], dane[feature_2], c=ms.labels_, cmap='viridis') 
     plt.scatter(cluster_centers[:, 0], cluster_centers[:, 1], marker='o', s=300, 
                 edgecolor='k', facecolor='none') 
     plt.title('Metoda przesunięcia średniej (Mean Shift)') 
@@ -110,9 +92,6 @@ def Mean_Shift_clustering(dane, feature_1: str, feature_2: str):
 
 def Agglomerative_clustering(dane, feature_1: str, feature_2: str, n_clusters):
 
-    # Wybór cech do grupowania 
-    X = dane[[feature_1, feature_2]]
-
     n = len(n_clusters) if isinstance(n_clusters, list) else 1
     n_rows = n//2 if n % 2 == 0 else (n//2) + 1
 
@@ -122,12 +101,12 @@ def Agglomerative_clustering(dane, feature_1: str, feature_2: str, n_clusters):
     for i, k in enumerate(n_clusters if isinstance(n_clusters, list) else [n_clusters]):
         # Implementacja klastrowania aglomeracyjnego 
         ac = AgglomerativeClustering(n_clusters=k) 
-        ac.fit(X) 
+        ac.fit(dane) 
         # Przewidywanie przynależności do klastrów dla każdej próbki 
         labels = ac.labels_ 
     
         # Wyświetlenie wyników 
-        axes[i].scatter(X[feature_1], X[feature_2], c=labels, cmap='viridis', alpha=0.5)  
+        axes[i].scatter(dane[feature_1], dane[feature_2], c=labels, cmap='viridis', alpha=0.5)  
         axes[i].set_xlabel(feature_1) 
         axes[i].set_ylabel(feature_2) 
         axes[i].set_title(f'K = {k}')
@@ -136,7 +115,79 @@ def Agglomerative_clustering(dane, feature_1: str, feature_2: str, n_clusters):
     plt.tight_layout()
     plt.suptitle('Klastrowanie aglomeracyjne (Agglomerative Clustering)')
     plt.show()
+
+
+def Gaussian_Mixture_clustering(dane, feature_1: str, feature_2: str, n_clusters):
+    
+    n = len(n_clusters) if isinstance(n_clusters, list) else 1
+    n_rows = n//2 if n % 2 == 0 else (n//2) + 1
+
+    fig, axes = plt.subplots(n_rows, 2, figsize=(12, 6))
+    axes = axes.flatten()
+    
+    for i, k in enumerate(n_clusters if isinstance(n_clusters, list) else [n_clusters]):
+        # Implementacja klastrowania z użyciem mieszanki Gaussa 
+        gmm = GaussianMixture(n_components=k) 
+        gmm.fit(dane) 
+        labels = gmm.predict(dane)
+    
+        # Wyświetlenie wyników 
+        axes[i].scatter(dane[feature_1], dane[feature_2], c=labels, cmap='viridis', alpha=0.5)  
+        axes[i].set_xlabel(feature_1) 
+        axes[i].set_ylabel(feature_2) 
+        axes[i].set_title(f'K = {k}')
+        axes[i].grid(True)
+
+    plt.tight_layout()
+    plt.suptitle('Klastrowanie z użyciem mieszanki Gaussa')
+    plt.show()
  
+
+def DBSCAN_clustering(dane):
+
+    # Implementacja DBSCAN 
+    dbscan = DBSCAN(eps=0.5, min_samples=5) 
+    clusters = dbscan.fit_predict(dane) 
+    
+    print(f"ilość klastrów: {len(set(clusters))}")
+    # Redukcja wymiarów do wizualizacji 
+    pca = PCA(n_components=2).fit(dane) 
+    X_pca = pca.transform(dane) 
+    
+    # Zwizualizowanie wyników 
+    plt.figure(figsize=(10, 7)) 
+    plt.scatter(X_pca[:, 0], X_pca[:, 1], c=clusters, cmap='viridis') 
+    plt.title('DBSCAN') 
+    plt.xlabel('PCA 1') 
+    plt.ylabel('PCA 2') 
+    plt.colorbar(label='Klastry') 
+    plt.show()
+
+
+def hierarchical_clustering(dane):
+    # Implementacja klastrowania hierarchicznego 
+    linked = linkage(dane, method='ward') 
+    
+    # Wyświetlenie dendrogramu 
+    plt.figure(figsize=(10, 7)) 
+    dendrogram(linked, 
+               orientation='top', 
+               distance_sort='descending', 
+               show_leaf_counts=True) 
+    plt.title('Dendrogram Hierarchicznego Klastrowania') 
+    plt.xlabel('Indeksy próbek') 
+    plt.ylabel('Odległość') 
+    plt.show()
+
+
+def ocena_jakosci_klastrowania(dane, labels, nazwa_metody):
+    sil_score = silhouette_score(dane, labels)
+    ch_score = calinski_harabasz_score(dane, labels)
+    db_score = davies_bouldin_score(dane, labels)
+    print(f"\nMetoda: {nazwa_metody}")
+    print(f"Silhouette Score: {sil_score:.3f}")
+    print(f"Calinski-Harabasz Score: {ch_score:.3f}")
+    print(f"Davies-Bouldin Score: {db_score:.3f}")
 
 
 
@@ -150,15 +201,16 @@ credit_card_data = credit_card_data.drop_duplicates()
 # Kodowanie zmiennych kategorycznych
 credit_card_data = kodowanie_zmiennych_kategorycznych(credit_card_data)
 
+
 # Zadanie 2
-# Wybór cech do klasteryzacji
+# Wybór cech do wykresu 2D
 feature_1 = 'LIMIT_BAL'
 feature_2 = 'BILL_AMT1'
 
 # Przykładowe liczby klastrów do przetestowania
 n_clusters = [2, 3, 4, 5]
 
-# K_Means_clustering(credit_card_data, feature_1, feature_2, n_clusters)
+K_Means_clustering(credit_card_data, feature_1, feature_2, n_clusters)
 
 # Zadanie 3
 # Standardyzacja danych
@@ -167,7 +219,7 @@ feature_columns = [feature_1, feature_2]
 credit_card_data[feature_columns] = scaler.fit_transform(credit_card_data[feature_columns])
 
 # Klasteryzacja z użyciem Mean Shift
-# Mean_Shift_clustering(credit_card_data, feature_1, feature_2)
+Mean_Shift_clustering(credit_card_data, feature_1, feature_2)
 
 
 # Zadanie 4
@@ -178,13 +230,112 @@ iris_data = iris_data.dropna()
 iris_data = iris_data.drop_duplicates()
 iris_data = kodowanie_zmiennych_kategorycznych(iris_data)
 
-# Wybór cech do klasteryzacji
+# Wybór cech do wykresu 2D
 feature_1 = 'sepal length (cm)'
-feature_2 = 'sepal width (cm)'
+feature_2 = 'petal length (cm)'
 
 # Klasteryzacja aglomeracyjna
 Agglomerative_clustering(iris_data, feature_1, feature_2, n_clusters=n_clusters)
 
 
+# Zadanie 5
+# Wczytanie zbioru danych Wine
+wine = load_wine()
+wine_data = pd.DataFrame(data=wine.data, columns=wine.feature_names)
+
+wine_data = wine_data.dropna()
+wine_data = wine_data.drop_duplicates()
+wine_data = kodowanie_zmiennych_kategorycznych(wine_data)
+
+# Wybór cech do wykresu 2D
+feature_1 = 'malic_acid'
+feature_2 = 'color_intensity'
+
+Gaussian_Mixture_clustering(wine_data, feature_1, feature_2, n_clusters=n_clusters)
 
 
+# Zadanie 6
+# Wczytanie zbioru danych Olivetti Faces
+faces = fetch_olivetti_faces()
+faces_data = pd.DataFrame(data=faces.data)
+faces_data = faces_data.dropna()
+faces_data = faces_data.drop_duplicates()
+faces_data = kodowanie_zmiennych_kategorycznych(faces_data)
+
+DBSCAN_clustering(faces_data)
+
+# Zadanie 7
+diabetes = load_diabetes()
+diabetes_data = pd.DataFrame(data=diabetes.data, columns=diabetes.feature_names)
+diabetes_data = diabetes_data.dropna()
+diabetes_data = diabetes_data.drop_duplicates()
+diabetes_data = kodowanie_zmiennych_kategorycznych(diabetes_data)
+
+hierarchical_clustering(diabetes_data)
+
+# Zadanie 8
+heart_data = pd.read_csv('lista 7\heart_failure_clinical_records_dataset.csv')
+heart_data = heart_data.dropna()
+heart_data = heart_data.drop_duplicates()
+heart_data = kodowanie_zmiennych_kategorycznych(heart_data)
+
+# Wybór cech do wykresu 2D
+feature_1 = 'platelets'
+feature_2 = 'creatinine_phosphokinase'
+
+# Klasteryzacja KMeans
+K_Means_clustering(heart_data, feature_1, feature_2, n_clusters=n_clusters)
+
+# Klasteryzacja aglomeracyjna
+Agglomerative_clustering(heart_data, feature_1, feature_2, n_clusters=n_clusters)
+
+# Klasteryzacja z użyciem mieszanki Gaussa
+Gaussian_Mixture_clustering(heart_data, feature_1, feature_2, n_clusters=n_clusters)
+
+# Klasteryzacja DBSCAN
+DBSCAN_clustering(heart_data)
+
+# Przygotowanie danych do oceny (tylko wybrane cechy)
+feature_columns = [feature_1, feature_2]
+X = heart_data[feature_columns]
+
+# KMeans
+kmeans = KMeans(n_clusters=3, random_state=42)
+kmeans_labels = kmeans.fit_predict(X)
+ocena_jakosci_klastrowania(X, kmeans_labels, "KMeans")
+
+# Agglomerative Clustering
+agg = AgglomerativeClustering(n_clusters=3)
+agg_labels = agg.fit_predict(X)
+ocena_jakosci_klastrowania(X, agg_labels, "Agglomerative Clustering")
+
+# Gaussian Mixture
+gmm = GaussianMixture(n_components=3, random_state=42)
+gmm_labels = gmm.fit(X).predict(X)
+ocena_jakosci_klastrowania(X, gmm_labels, "Gaussian Mixture")
+
+# DBSCAN
+dbscan = DBSCAN(eps=0.5, min_samples=5)
+dbscan_labels = dbscan.fit_predict(X)
+# Sprawdź, czy DBSCAN znalazł więcej niż 1 klaster (poza szumem)
+if len(set(dbscan_labels)) > 1 and -1 in dbscan_labels:
+    mask = dbscan_labels != -1
+    unique_labels = set(dbscan_labels[mask])
+    if len(unique_labels) > 1:
+        ocena_jakosci_klastrowania(X[mask], dbscan_labels[mask], "DBSCAN (bez szumu)")
+    else:
+        print("\nDBSCAN: znaleziono tylko jeden klaster po odfiltrowaniu szumu - metryki nie mają sensu.")
+elif len(set(dbscan_labels)) > 1:
+    ocena_jakosci_klastrowania(X, dbscan_labels, "DBSCAN")
+else:
+    print("\nDBSCAN: znaleziono tylko jeden klaster lub same szumy - metryki nie mają sensu.")
+
+print("""
+Interpretacja:
+- Wyższy Silhouette Score i Calinski-Harabasz Score oznaczają lepsze rozdzielenie i zwartość klastrów.
+- Niższy Davies-Bouldin Score oznacza lepszą jakość klastrowania.
+- Najlepsza metoda to ta, która osiąga najwyższy Silhouette i Calinski-Harabasz oraz najniższy Davies-Bouldin.
+- W kontekście medycznym, metoda powinna tworzyć wyraźnie oddzielone grupy, które mogą odpowiadać różnym profilom klinicznym pacjentów.
+      
+Najlepsze wyniki uzyskano dla KMeans i Agglomerative Clustering, które wykazały najlepsze metryki jakości klastrowania.
+""")
